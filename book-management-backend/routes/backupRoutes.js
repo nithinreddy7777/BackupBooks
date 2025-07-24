6946ed8c60b27d934ea4fd222f56e2f7718207de
nithinreddy7777 <nnithinreddy27@gmail.com>
2025-07-24 21:01:48 +0530

    Update backupRoutes.js

--- book-management-backend/routes/backupRoutes.js
+++ book-management-backend/routes/backupRoutes.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { backupDatabase, restoreDatabase } = require('../controllers/backupController');
-
-// POST /api/backup
 router.post('/backup', backupDatabase);
-
-// POST /api/restore
 router.post('/restore', restoreDatabase);
-
 module.exports = router;
